64768faaf0ea55df82fa0659814eefb432a53c70
Michelangela Paesano <paesanomichelangela@gmail.com>
2025-05-20 18:27:40 +0200

    fine esercizio

--- js/paridispari.js
+++ js/paridispari.js
@@ -1,6 +1,8 @@
+//creiamo prima il prompt per far mettere il numero al cliente 
 const numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5'))
 console.log (numeroUtente)
 
+//con la funzione che ho trovato su MDN faccio generare un numero al pc 
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
@@ -10,5 +12,18 @@ function getRandomInt(min, max) {
 let numPc = getRandomInt(1, 6)
 console.log (numPc)
 
+//facciamo la somma delle due
 let somma = numeroUtente + numPc;
 console.log (`${numeroUtente} + ${numPc} = ${somma}`)
+
+//creiamo una funzione per decretare chi vince
+function isEven (num) {
+    if (somma % 2 === 0) {
+        console.log('il pc ha vinto')
+    } else {
+        console.log ('Il cliente ha vinto')
+    }
+
+}
+
+isEven(somma);
